2b50be8a91c2da10f964557cc28320c7bada41e2
EduSantos20 <eduardo_dasilvasantos18@outlook.com>
2021-11-16 01:44:41 -0300

    Moratona do discover dev finances na Rockseat

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+var modal = {
+  open() {
+    //abrir modal
+    //adicionar a class activo ao modal
+    alert('Abrir o modal')
+  },
+  close() {
+    //fechar o model
+    //preciso remover a class active do modal
+  }
+}
